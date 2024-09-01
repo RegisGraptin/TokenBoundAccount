c44be821c3232a5106d368f3a80a84cf523ccb18
rere <regis-graptin@sfr.fr>
2024-09-01 10:56:47 +0200

    feat: create API folder

---
+++ api/README.md
@@ -0,0 +1,8 @@
+
+## Run the server
+
+```bash
+node api.js
+```
+
+Will open the port `3001` by default.
---
+++ api/api.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+// ERC721 - ABI
+const TSHIRT_ABI = require("../contract/artifacts/contracts/TShirtNFT.sol/TShirtNFT.json");
+
+// Server Key - T-Shirt company
+const SERVER_KEY = process.env.SERVER_KEY;
+
+// NFT Contract Address
+const NFT_CONTRACT_ADDRESS = process.env.TSHIRT_NFT_CONTRACT_ADDRESS;
+
+const app = express();
+app.use(express.json());
+
+
+app.get("/status", (request, response) => {
+    const status = {
+        "Status": "Running"
+    };
+    
+    response.send(status);
+ });
+
+
+
+app.get("/create/:user_address", async (request, response) => {
+
+    try {
+        let user_address = req.params['user_address'];
+
+        let provider = new ethers.JsonRpcProvider("https://spicy-rpc.chiliz.com/");
+
+        let signer = new ethers.Wallet(SERVER_KEY, provider);
+
+        let contract = new ethers.Contract(
+            NFT_CONTRACT_ADDRESS,
+            TSHIRT_ABI.abi,
+            signer
+        );
+
+        let response = await contract.buyTShirt(user_address);
+
+        console.log(response);
+    
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error when minting a new NFT.');
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
